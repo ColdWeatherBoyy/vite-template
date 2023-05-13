775e075a29b78ae74a8a15eb74e5e191f04f146e
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-05-13 17:01:44 -0400

    adds basic routing and test pages

---
+++ client/src/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = () => {
+	return (
+		<>
+			<h1>404 Error</h1>
+			<p>Page not found</p>
+		</>
+	);
+};
+
+export default ErrorPage;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import TestPage from "./TestPage.jsx";
+import ErrorPage from "./ErrorPage.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
+		errorElement: <ErrorPage />,
 	},
 	{
 		path: "/test",
