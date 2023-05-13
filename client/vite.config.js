354af9c41e13a2639d9eaa03eca7394e92c01bea
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-05-13 17:20:06 -0400

    adds proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api/yelp": {
+				target: "http://localhost:3001",
+				changeOrigin: true,
+			},
+		},
+	},
+});
